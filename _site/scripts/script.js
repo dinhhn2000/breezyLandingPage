d9f077dc44e8e71931b2122fef706433307df8dc
WzZzW <dinhhongoc@gmail.com>
2019-05-10 06:38:41 +0700

    dev-page & apply-page

---
+++ _site/scripts/script.js
@@ -0,0 +1,37 @@
+// Handle position & education input
+function positionInputShow() {
+    let input = document.getElementsByClassName("position-input")[0];
+    let link = document.getElementsByClassName("position-input-link")[0];
+    let del = document.getElementsByClassName("position-delete-link")[0];
+    // console.log(input);
+    input.style.display = "block";
+    link.style.display = "none";
+    del.style.display = "block";
+}
+function positionInputHide() {
+    let input = document.getElementsByClassName("position-input")[0];
+    let link = document.getElementsByClassName("position-input-link")[0];
+    let del = document.getElementsByClassName("position-delete-link")[0];
+    // console.log(input);
+    input.style.display = "none";
+    link.style.display = "block";
+    del.style.display = "none";
+}
+function educationInputShow() {
+    let input = document.getElementsByClassName("education-input")[0];
+    let link = document.getElementsByClassName("education-input-link")[0];
+    let del = document.getElementsByClassName("education-delete-link")[0];
+    // console.log(input);
+    input.style.display = "block";
+    link.style.display = "none";
+    del.style.display = "block";
+}
+function educationInputHide() {
+    let input = document.getElementsByClassName("education-input")[0];
+    let link = document.getElementsByClassName("education-input-link")[0];
+    let del = document.getElementsByClassName("education-delete-link")[0];
+    // console.log(input);
+    input.style.display = "none";
+    link.style.display = "block";
+    del.style.display = "none";
+}
